81afd1a17f9b0afa8ded09712db2507a5a30267d
Rusakov Nikita <lalkogad@gmail.com>
2026-02-13 17:42:06 +0300

    feat: added repl

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -28,7 +28,7 @@ docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
 try {
   rs.initiate({
     _id: "shard1",
-    members: [{ _id: 0, host: "shard1:27018" }]
+    members: [{ _id: 0, host: "shard1:27018" }, { _id: 1, host: "shard1_replica1:27018" }, { _id: 2, host: "shard1_replica2:27018" }]
   });
   print("Shard 1 initiated");
 } catch (e) { print("Shard 1 already initiated"); }
@@ -40,7 +40,7 @@ docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
 try {
   rs.initiate({
     _id: "shard2",
-    members: [{ _id: 0, host: "shard2:27019" }]
+    members: [{ _id: 0, host: "shard2:27019" }, { _id: 1, host: "shard2_replica1:27019" },{ _id: 2, host: "shard2_replica2:27019" }]
   });
   print("Shard 2 initiated");
 } catch (e) { print("Shard 2 already initiated"); }
@@ -88,3 +88,27 @@ echo -n "Документов на Shard 2 (порт 27019): "
 docker compose exec -T shard2 mongosh --port 27019 --quiet --eval 'db.getSiblingDB("somedb").helloDoc.countDocuments()'
 
 echo "--- Проверка шардирования завершена ---"
+
+echo "--- Проверка количества реплик в шардах ---"
+
+# Проверка Shard 1
+echo -n "Количество узлов в Shard 1 (RS): "
+docker compose exec -T shard1 mongosh --port 27018 --quiet --eval "rs.status().members.length"
+docker compose exec -T shard1 mongosh --port 27018 --quiet --eval "
+  rs.status().members.forEach(function(m) { 
+    print('  - ' + m.name + ' [' + m.stateStr + ']'); 
+  })
+"
+
+echo "------------------------------------------"
+
+# Проверка Shard 2
+echo -n "Количество узлов в Shard 2 (RS): "
+docker compose exec -T shard2 mongosh --port 27019 --quiet --eval "rs.status().members.length"
+docker compose exec -T shard2 mongosh --port 27019 --quiet --eval "
+  rs.status().members.forEach(function(m) { 
+    print('  - ' + m.name + ' [' + m.stateStr + ']'); 
+  })
+"
+
+echo "--- Скрипт полностью выполнен ---"
